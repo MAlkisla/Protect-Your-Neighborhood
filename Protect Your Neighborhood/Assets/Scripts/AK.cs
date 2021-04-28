6967295c31d2ddb73b6379f0f0038e2b04c475aa
Meriç ALKIŞLA <mericalkisla@gmail.com>
2021-04-28 18:12:41 +0300

    Magazine system added

--- Protect Your Neighborhood/Assets/Scripts/AK.cs
+++ Protect Your Neighborhood/Assets/Scripts/AK.cs
@@ -1,39 +1,71 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class AK : MonoBehaviour
 {
+    Animator animator;
+
     public bool canShoot;
     float inFrequencyOfShoot;
     public float outFrequencyOfShoot;
     public float range;
-    public Camera myCam;
+
+    [Header("Sounds")]
     public AudioSource fireSound;
     public AudioSource magazineChargerSound;
+    public AudioSource bulletOutSound;
+
+    [Header("Effects")]
     public ParticleSystem fireEffect;
     public ParticleSystem bulletTrace;
     public ParticleSystem bloodEffect;
 
-    Animator animator;
+    [Header("Others")]
+    public Camera myCam;
+
+    [Header("Gun Settings")]
+    public int TotalBulletQuantity;
+    public int MagazineCapacity;
+    public int RemainingBullets;
+    public TextMeshProUGUI TotalBullet_Text;
+    public TextMeshProUGUI RemainingBullets_Text;
+
 
     void Start()
     {
+        RemainingBullets = MagazineCapacity;
+        MagazineCharging("Normal");
         animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse0) && canShoot && Time.time > inFrequencyOfShoot)
+        if (Input.GetKey(KeyCode.Mouse0))
         {
-            Shoot();
-            inFrequencyOfShoot = Time.time + outFrequencyOfShoot;
+            if (canShoot && Time.time > inFrequencyOfShoot && RemainingBullets != 0)
+            {
+                Shoot();
+                inFrequencyOfShoot = Time.time + outFrequencyOfShoot;
+            }
+            if (RemainingBullets == 0)
+                bulletOutSound.Play();
+
         }
         if (Input.GetKey(KeyCode.R))
         {
-            animator.Play("magazinecharger");
+            if (RemainingBullets < MagazineCapacity && TotalBulletQuantity != 0)
+            {
+                if (RemainingBullets != 0)
+                    MagazineCharging("BulletsAvailable");
+                else
+                    MagazineCharging("BulletsIsNotAvailable");
+
+                animator.Play("magazinecharger");
+            }
         }
     }
 
@@ -47,6 +79,9 @@ public class AK : MonoBehaviour
         fireSound.Play();
         fireEffect.Play();
         animator.Play("fire");
+
+        RemainingBullets--;
+        RemainingBullets_Text.text = RemainingBullets.ToString();
 
         RaycastHit hit;
 
@@ -65,6 +100,47 @@ public class AK : MonoBehaviour
 
             else
                 Instantiate(bulletTrace, hit.point, Quaternion.LookRotation(hit.normal));
+        }
+    }
+
+    private void MagazineCharging(string stage)
+    {
+        switch (stage)
+        {
+            case "BulletsAvailable":
+                if (TotalBulletQuantity <= MagazineCapacity)
+                {
+                    int currentBullets = RemainingBullets;
+                    RemainingBullets += TotalBulletQuantity;
+                }
+                else
+                {
+                    TotalBulletQuantity -= MagazineCapacity - RemainingBullets;
+                    RemainingBullets = MagazineCapacity;
+                }
+                TotalBullet_Text.text = TotalBulletQuantity.ToString();
+                RemainingBullets_Text.text = RemainingBullets.ToString();
+                break;
+
+            case "BulletsIsNotAvailable":
+                if (TotalBulletQuantity <= MagazineCapacity)
+                {
+                    RemainingBullets = TotalBulletQuantity;
+                    TotalBulletQuantity = 0;
+                }
+                else
+                {
+                    TotalBulletQuantity -= MagazineCapacity;
+                    RemainingBullets = MagazineCapacity;
+                }
+                TotalBullet_Text.text = TotalBulletQuantity.ToString();
+                RemainingBullets_Text.text = RemainingBullets.ToString();
+                break;
+
+            case "Normal":
+                TotalBullet_Text.text = TotalBulletQuantity.ToString();
+                RemainingBullets_Text.text = RemainingBullets.ToString();
+                break;
         }
     }
 }
